c7f031723210f3e39206488db16b698ebf33a051
dugustlr@naver.com <dugustlr@naver.com>
2024-08-20 19:57:16 +0900

    설정 파일의 도입
    - webpack은 일일히 커맨드로 칠 필요없이 설정 파일을 통해서 손쉽게 실행시킬 수 있음
    - 'npx webpack --config webpack.config.js' 또는 'npx webpack'으로 실행

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require("path");
+
+/** 'npx webpack --entry ./src/index.js --output-path ./dist' << 얘랑 같음 */
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    // 결과물을 어떤 경로에 저장할 것인지
+    path: path.resolve(__dirname, "dist"),
+    // 파일 이름 지정
+    filename: "main.js",
+  },
+};
