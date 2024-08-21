46a80ac681cf44279f3a15b54e44a02ab8fb1269
dugustlr@naver.com <dugustlr@naver.com>
2024-08-21 18:41:29 +0900

    여러 개의 output을 만드는 방법
    - 설정 파일에서 entry 속성을 객체로 바꿔준 다음 만들고자 하는 output 만큼 속성을 추가해주면 됨
    - output의 filename 속성의 값을 지정할 때, '[name]' 예약어를 사용하면 entry에 지정한 속성 키가 자동으로 적용되어 각 entry마다의 output이 만들어짐
    
    [+]
    - 'about.html' 파일 추가
    - 'src/about.js' 파일 추가

---
+++ src/about.js
@@ -0,0 +1,4 @@
+import css from "./style.css";
+
+const title = document.querySelector("h1#title");
+title.innerText = "ABOUT";
--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,15 @@ const path = require("path");
 /** 'npx webpack --entry ./src/index.js --output-path ./dist' << 얘랑 같음 */
 module.exports = {
   mode: "development",
-  entry: "./src/index.js",
+  entry: {
+    main: "./src/index.js",
+    about: "./src/about.js",
+  },
   output: {
     // 결과물을 어떤 경로에 저장할 것인지
     path: path.resolve(__dirname, "dist"),
     // 파일 이름 지정
-    filename: "main.js",
+    filename: "[name].js",
   },
   module: {
     rules: [
